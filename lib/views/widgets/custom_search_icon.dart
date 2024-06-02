d3b5e92b13d4d4e3880b2cb9813b20bddf252764
ahmedayman44 <ahmedayman203011@gmail.com>
2024-06-02 15:18:01 +0300

    create notes view body for all widget

---
+++ lib/views/widgets/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 47,
+        width: 47,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.white.withOpacity(.05)),
+        child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 30,
+            )));
+  }
+}
